c184d78af26c73491ab3646d1c24039980e7f31b
amna <amnaa.qader114@gmail.com>
2026-09-17 10:30:07 +0300

    last week and month filtering methods are  done

--- src/com/ga/bank/FileManager.java
+++ src/com/ga/bank/FileManager.java
@@ -210,4 +210,30 @@ public class FileManager {
              return filteredList;
          }
 
+
+        public static List<String> filterLastWeek(Customer customer) throws IOException {
+        List<String>  list = readTransactions(customer);
+
+        List<String>  filteredList = list.stream().filter( trans->{
+            String[] parts = trans.split(",");
+            LocalDate transactionDate = LocalDate.parse(parts[0]);
+            return transactionDate.isEqual(LocalDate.now().minusWeeks(1));
+        }).toList();
+
+        return filteredList;
+        }
+
+
+    public static List<String> filterLastMonth(Customer customer) throws IOException {
+        List<String>  list = readTransactions(customer);
+
+        List<String>  filteredList = list.stream().filter( trans->{
+            String[] parts = trans.split(",");
+            LocalDate transactionDate = LocalDate.parse(parts[0]);
+            return transactionDate.isEqual(LocalDate.now().minusMonths(1));
+        }).toList();
+
+        return filteredList;
+    }
+
 }
